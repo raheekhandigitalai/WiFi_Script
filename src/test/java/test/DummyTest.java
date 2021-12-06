b5144bf2435612714c3da0ebd2e81c708fdf155d
raheekhandigitalai <rahee.khan@digital.ai>
2021-12-06 13:05:05 -0500

    [#9] - Ugh

--- src/test/java/test/DummyTest.java
+++ src/test/java/test/DummyTest.java
@@ -1,7 +1,41 @@
 package test;
 
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.testng.annotations.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DummyTest {
 
+    private String accessKey = "eyJ4cC51Ijo3MzU0MjQsInhwLnAiOjIsInhwLm0iOiJNVFUzT0RZd016ZzFOek16TVEiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE4OTM5NjM4NTcsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.GP0hK0o0j2WEKt-J0aXsVbu1tmt-PhWUryqluokszJk";
+    protected IOSDriver<IOSElement> driver = null;
+    DesiredCapabilities dc = new DesiredCapabilities();
 
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        dc.setCapability("accessKey", accessKey);
+        dc.setCapability("deviceQuery", "@os='ios' and @category='PHONE'");
+        dc.setCapability("releaseDevice", false);
+        dc.setCapability(IOSMobileCapabilityType.BUNDLE_ID, "com.experitest.ExperiBank");
+        driver = new IOSDriver<>(new URL("https://uscloud.experitest.com/wd/hub"), dc);
+    }
 
+    @Test
+    public void quickStartiOSNativeDemo() {
+        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='loginButton']")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
